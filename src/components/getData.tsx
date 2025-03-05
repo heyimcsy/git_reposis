2b8a2ab6ed4470d4a098912a35ea857f068562a0
sychoi <cinnamon1995@naver.com>
2025-03-06 01:52:16 +0900

    add function of getting information of github api

--- src/components/getData.tsx
+++ src/components/getData.tsx
@@ -8,7 +8,7 @@ export default function GetData(){
     const [totalCount, setTotalCount] = useState<number>(0);
     const [results, setResults] = useState<any[]>([]);
     const getResults = async () => {
-        return await axios.get(`https://api.github.com/search/repositories?q={next}`)
+        return await axios.get(`https://api.github.com/search/repositories?q={map}`)
     }
     const {data, isSuccess} = useQuery({
         queryKey:['github'],
@@ -19,12 +19,25 @@ export default function GetData(){
         if (!isSuccess) return
         setTotalCount(data?.data.total_count)
         setResults(data?.data.items)
-        console.log(totalCount,'dataaa',results)
+        console.log(results)
     }, [data]);
 
     return (
-        <div style={{width: '900px', height: '900px',border: '1px solid pink' }}>
-
+        <div style={{width: '900px', height: '100%',border: '1px solid pink' }}>
+            <p>Total Counts : <span>{totalCount}</span></p>
+            <div style={{width: '700px', height: '700px', border:'1px solid red',
+            display: 'flex',overflow: 'auto' }}>
+                {results.map((data) => (
+                    <div key={data.id} style={{width: '400px', height: ' 350px', border: '1px solid orange'}}>
+                        <p style={{width: '400px', fontSize: '26px'}}>name: {data.name}</p>
+                        <p style={{width: '400px'}}>description: {data.description}</p>
+                        <p style={{width: '400px'}}>owner: {data.owner.login}</p>
+                        <p style={{width: '400px'}}>language: {data.language}</p>
+                        {/*<a href={data.downloads_url}>downloads_url: {data.downloads_url} </a>*/}
+                    </div>
+                ))
+                }
+            </div>
         </div>
     )
 }

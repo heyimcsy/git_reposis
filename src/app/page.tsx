14cc89af0612e8034f1a915fe5abd42f24ea5726
sychoi <cinnamon1995@naver.com>
2025-03-06 01:59:57 +0900

    remove unused function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 import styles from "./page.module.css";
-import {useState} from "react";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtoolsPanel} from "@tanstack/react-query-devtools";
 import GetData from "@/components/getData";
+import Search from "@/components/search";
 
 export default function Home() {
-    const [open, setOpen] = useState<boolean>(false)
     const queryClient:QueryClient = new QueryClient();
 
   return (
@@ -15,13 +13,11 @@ export default function Home() {
           <div style={{width: '100%', height: '100%'}}>
               <div>
                 <h1> HOME</h1>
+                  <Search/>
                   <GetData/>
               </div>
               <footer className={styles.footer}>
-                  <button
-                      onClick={() => setOpen(!open)}
-                  >{`${open ? 'Close' : 'Open'} the devtools panel`}</button>
-                  {open && <ReactQueryDevtoolsPanel onClose={() => setOpen(false)} />}
+
               </footer>
           </div>
 
